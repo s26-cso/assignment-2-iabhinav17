15eb41de862371c201e729fa418fc51256f0ad59
Abhinav <iabhinav17@gmail.com>
2026-04-16 21:06:56 +0530

    q4 change

--- q4/q4.c
+++ q4/q4.c
@@ -7,33 +7,34 @@ int main()
     char op[100];
     int a, b;
 
-    scanf("%s %d %d", op, &a, &b);
+    while(scanf("%s %d %d", op, &a, &b)==3){
 
-    char libname[100] = "lib";
-    strcat(libname, op);
-    strcat(libname, ".so");
+        char libname[100] = "lib";
+        strcat(libname, op);
+        strcat(libname, ".so");
 
-    char path[120] = "./";
-    strcat(path, libname);
+        char path[120] = "./";
+        strcat(path, libname);
 
-    void *handle = dlopen(path, RTLD_LAZY);
-    if (!handle) {
-        printf("Error: %s\n", dlerror());
-        return 1;
+        void *handle = dlopen(path, RTLD_LAZY);
+        if (!handle) {
+            printf("Error: %s\n", dlerror());
+            return 1;
+        }
+
+        int (*func)(int,int);
+        func = dlsym(handle, op);
+        if (!func) {
+            printf("Error: %s\n", dlerror());
+            return 1;
+        }
+
+        int result = func(a, b);
+
+        printf("%d\n", result);
+
+        dlclose(handle);
+
     }
-
-    int (*func)(int,int);
-    func = dlsym(handle, op);
-    if (!func) {
-        printf("Error: %s\n", dlerror());
-        return 1;
-    }
-
-    int result = func(a, b);
-
-    printf("%d\n", result);
-
-    dlclose(handle);
-
     return 0;
 }
